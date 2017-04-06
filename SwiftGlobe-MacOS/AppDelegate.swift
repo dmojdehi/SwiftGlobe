2edf228707b3e42e2594a816b6148900efecfb79
David <dmojdehi@gmail.com>
2017-04-06 13:00:06 -0700

    added macos target (also removed test projects)

---
+++ SwiftGlobe-MacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftGlobe-MacOS
+//
+//  Created by David Mojdehi on 4/6/17.
+//  Copyright © 2017 Mindful Bear Apps. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftGlobe-MacOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftGlobe-MacOS
+//
+//  Created by David Mojdehi on 4/6/17.
+//  Copyright © 2017 Mindful Bear Apps. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
