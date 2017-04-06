3c0bd5f71053a2a39f93e6d19e7fe950b9dfbead
David <dmojdehi@gmail.com>
2017-04-06 11:35:57 -0700

    Initial Commit

---
+++ SwiftGlobe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftGlobe
+//
+//  Created by David Mojdehi on 4/6/17.
+//  Copyright © 2017 Mindful Bear Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
