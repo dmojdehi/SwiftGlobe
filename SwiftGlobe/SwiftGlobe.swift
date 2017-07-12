00e5e0b0e0ac835efd9ea02e83d43bef1b4b2456
Moj <dmojdehi@gmail.com>
2017-07-12 13:24:50 -0700

    move utility classes lower in the .swift file

--- SwiftGlobe/SwiftGlobe.swift
+++ SwiftGlobe/SwiftGlobe.swift
@@ -35,77 +35,6 @@ let kDaysInAYear = 365.0
 
 let kAffectedBySpring = 1 << 1
 
-extension SCNVector3 {
-    init(x: Double, y: Double, z:Double ){
-        #if os(iOS) || os(tvOS)
-            self.init(x: Float(x), y: Float(y), z: Float(z))
-        #elseif os(OSX)
-            self.init(x: CGFloat(x), y: CGFloat(y), z: CGFloat(z))
-        #endif
-    }
-}
-
-class GlobeGlowPoint {
-    var latitude = 0.0
-    var longitude = 0.0
-    
-    // the node of this point (once it has been added to the scene)
-    fileprivate var node : SCNNode!
-    
-    init(lat: Double, lon: Double) {
-        self.latitude = lat
-        self.longitude = lon
-        
-        self.node = SCNNode(geometry: SCNPlane(width: kGlowPointWidth, height: kGlowPointWidth) )
-        self.node.geometry!.firstMaterial!.diffuse.contents = "yellowGlow-32x32.png"
-        // appear a little washed out in daylight...
-        self.node.geometry!.firstMaterial!.diffuse.intensity = 0.2
-        self.node.geometry!.firstMaterial!.emission.contents = "yellowGlow-32x32.png"
-        // but brigheter in dark areas
-        self.node.geometry!.firstMaterial!.emission.intensity = 0.7
-        self.node.castsShadow = false
-        
-        // NB: our textures *center* on 0,0, so adjust by 90 degrees
-        let adjustedLon = lon + 90
-
-        // convert lat & lon to xyz
-        // Note scenekit coordinate space:
-        //      Camera looks  down the Z axis (down from +z)
-        //      Right is +x, left is -x
-        //      Up is +y, down is -y
-        let cosLat = cos(lat * Double.pi / 180.0)
-        let sinLat = sin(lat * Double.pi / 180.0);
-        let cosLon = cos(adjustedLon * Double.pi / 180.0);
-        let sinLon = sin(adjustedLon * Double.pi / 180.0);
-        let x = kGlowPointAltitude * cosLat * cosLon;
-        let y = kGlowPointAltitude * cosLat * sinLon;
-        let z = kGlowPointAltitude * sinLat;
-        //
-        let sceneKitX = -x
-        let sceneKitY = z
-        let sceneKitZ = y
-        
-        //print("convered lat: \(lat) lon: \(lon) to \(sceneKitX),\(sceneKitY),\(sceneKitZ)")
-        
-        let pos = SCNVector3(x: sceneKitX, y: sceneKitY, z: sceneKitZ )
-        self.node.position = pos
-        
-        
-        // and compute the normal pitch, yaw & roll (facing away from the globe)
-        //1. Pitch (the x component) is the rotation about the node's x-axis (in radians)
-        let pitch = -lat * Double.pi / 180.0
-        //2. Yaw   (the y component) is the rotation about the node's y-axis (in radians)
-        let yaw = lon * Double.pi / 180.0
-        //3. Roll  (the z component) is the rotation about the node's z-axis (in radians)
-        let roll = 0.0
-        
-        
-        self.node.eulerAngles = SCNVector3(x: pitch, y: yaw, z: roll )
-        
-    }
-    
-}
-
 class SwiftGlobe {
     
     
@@ -568,6 +497,82 @@ class SwiftGlobe {
         
         cameraGoal.position = SCNVector3(x: newX, y: newY, z: newZ )
         
+        
+    }
+    
+}
+
+
+
+// simple extension to reduce platform-specific #if's (for some reason SCNVector uses CGFloat on macOS)
+extension SCNVector3 {
+    init(x: Double, y: Double, z:Double ){
+        #if os(iOS) || os(tvOS)
+            self.init(x: Float(x), y: Float(y), z: Float(z))
+        #elseif os(OSX)
+            self.init(x: CGFloat(x), y: CGFloat(y), z: CGFloat(z))
+        #endif
+    }
+}
+
+// code to encapsulate individual glow points
+// (extend this to get different glow effects)
+class GlobeGlowPoint {
+    var latitude = 0.0
+    var longitude = 0.0
+    
+    // the node of this point (must be added to the scene)
+    fileprivate var node : SCNNode!
+    
+    init(lat: Double, lon: Double) {
+        self.latitude = lat
+        self.longitude = lon
+        
+        self.node = SCNNode(geometry: SCNPlane(width: kGlowPointWidth, height: kGlowPointWidth) )
+        self.node.geometry!.firstMaterial!.diffuse.contents = "yellowGlow-32x32.png"
+        // appear a little washed out in daylight...
+        self.node.geometry!.firstMaterial!.diffuse.intensity = 0.2
+        self.node.geometry!.firstMaterial!.emission.contents = "yellowGlow-32x32.png"
+        // but brigheter in dark areas
+        self.node.geometry!.firstMaterial!.emission.intensity = 0.7
+        self.node.castsShadow = false
+        
+        // NB: our textures *center* on 0,0, so adjust by 90 degrees
+        let adjustedLon = lon + 90
+        
+        // convert lat & lon to xyz
+        // Note scenekit coordinate space:
+        //      Camera looks  down the Z axis (down from +z)
+        //      Right is +x, left is -x
+        //      Up is +y, down is -y
+        let cosLat = cos(lat * Double.pi / 180.0)
+        let sinLat = sin(lat * Double.pi / 180.0);
+        let cosLon = cos(adjustedLon * Double.pi / 180.0);
+        let sinLon = sin(adjustedLon * Double.pi / 180.0);
+        let x = kGlowPointAltitude * cosLat * cosLon;
+        let y = kGlowPointAltitude * cosLat * sinLon;
+        let z = kGlowPointAltitude * sinLat;
+        //
+        let sceneKitX = -x
+        let sceneKitY = z
+        let sceneKitZ = y
+        
+        //print("convered lat: \(lat) lon: \(lon) to \(sceneKitX),\(sceneKitY),\(sceneKitZ)")
+        
+        let pos = SCNVector3(x: sceneKitX, y: sceneKitY, z: sceneKitZ )
+        self.node.position = pos
+        
+        
+        // and compute the normal pitch, yaw & roll (facing away from the globe)
+        //1. Pitch (the x component) is the rotation about the node's x-axis (in radians)
+        let pitch = -lat * Double.pi / 180.0
+        //2. Yaw   (the y component) is the rotation about the node's y-axis (in radians)
+        let yaw = lon * Double.pi / 180.0
+        //3. Roll  (the z component) is the rotation about the node's z-axis (in radians)
+        let roll = 0.0
+        
+        
+        self.node.eulerAngles = SCNVector3(x: pitch, y: yaw, z: roll )
         
     }
     
